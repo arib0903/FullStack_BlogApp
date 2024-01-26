c9e135dac9cc0b0a6e76335e7acac0c40bed2c41
Arib Mahboob <mahboobarib@gmail.com>
2024-01-26 11:38:25 -0500

    Created the Login/Register forms

--- frontend-client/src/App.jsx
+++ frontend-client/src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from "./components/navbar.component";
 import {Routes, Route} from "react-router-dom";
+import UserAuthForm from "./pages/userAuthForm.page";
 const App = () => {
     return (
         <Routes>
             {/* We nest the Navbar route(Parent route) with the other routes so we are able to see the navbar on each of these pages */}
             <Route path="/" element={<Navbar />}>
-                <Route path="/signin" element={<h1>Sign In page</h1>} />
-                <Route path="/signup" element={<h1>Sign Up page</h1>} />
+                <Route path="/signin" element={<UserAuthForm type = "sign-in"></UserAuthForm>} />
+                <Route path="/signup" element={<UserAuthForm type = "sign-up"></UserAuthForm>} />
             </Route>
             
 
